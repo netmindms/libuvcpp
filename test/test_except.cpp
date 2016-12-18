cd556b8fab43eeb0f8c4cdbabb21d5f24fa90a1d
khkim <netmindms@home.com>
2016-12-19 02:13:09 +0900

    add inotify test.

--- test/test_except.cpp
+++ test/test_except.cpp
@@ -7,6 +7,7 @@
 #include "../uvcpp/UvTcp.h"
 #include "tlog.h"
 #include "../uvcpp/UvTimer.h"
+#include "../uvcpp/UvPoll.h"
 
 using namespace uvcpp;
 
@@ -74,3 +75,34 @@ TEST(except, timerrep) {
 	UvContext::run();
 	UvContext::close();
 }
+
+#ifdef __linux
+#include <sys/inotify.h>
+TEST(except, inotify) {
+	FILE* st = fopen("test.txt", "wb");
+	fclose(st);
+	UvContext::open();
+	UvPoll ipoll;
+	int fd = inotify_init();
+	inotify_add_watch(fd, "test.txt", IN_CLOSE_WRITE);
+	ipoll.init(fd);
+	ipoll.start(UV_READABLE, [&](int event) {
+		inotify_event evt;
+		::read(ipoll.getFd(), &evt, sizeof(evt));
+		ald("file closed on write");
+		ipoll.stop();
+		::close(fd);
+	});
+	st = fopen("test.txt", "a+b");
+	fwrite("1234", 1, 4, st);
+	UvTimer timer;
+	timer.init();
+	timer.start(1000, 1000, [&]() {
+		ald("timer stop");
+		timer.stop();
+		fclose(st);
+	});
+	UvContext::run();
+	UvContext::close();
+}
+#endif
